47eb0d1a74e9b795143d0e1a2c0301b7682305c6
Jason Lee <jason.ferrier+github@gmail.com>
2017-05-02 16:30:42 -0700

    Refactored to make code self-documenting

--- YouTube/YouTube-Playlist-Total-Length.js
+++ YouTube/YouTube-Playlist-Total-Length.js
@@ -1,33 +1,29 @@
 // Minified JS bookmarklet code created with http://mrcoles.com/bookmarklet/
-javascript:(function()%7Bvar%20sumMin%20%3D%200%2C%20sumSec%20%3D%200%2C%20timestamps%20%3D%20document.querySelectorAll('.timestamp%20span')%3Bfor%20(let%20i%3D0%3B%20i%20%3C%20timestamps.length%3B%20i%2B%2B)%20%7BsumMin%20%2B%3D%20parseInt(timestamps%5Bi%5D.innerText.split(%22%3A%22)%5B0%5D)%3BsumSec%20%2B%3D%20parseInt(timestamps%5Bi%5D.innerText.split(%22%3A%22)%5B1%5D)%3B%7Dif%20(sumSec%20%3E%3D%2060)%20%7BsumMin%20%2B%3D%20Math.floor(sumSec%20%2F%2060)%3BsumSec%20%3D%20sumSec%20%25%2060%3B%7Dalert(%22Total%20time%20of%20playlist%3A%20%22%20%2B%20sumMin%20%2B%20%22%3A%22%20%2B%20sumSec)%7D)()
+javascript:(function()%7Bvar%20totalNumberOfMinutes%20%3D%200%2C%20totalNumberOfSeconds%20%3D%200%2C%20timestamps%20%3D%20document.querySelectorAll('.timestamp%20span')%3Bfor%20(let%20i%3D0%3B%20i%20%3C%20timestamps.length%3B%20i%2B%2B)%20%7BtotalNumberOfMinutes%20%2B%3D%20parseInt(timestamps%5Bi%5D.innerText.split(%22%3A%22)%5B0%5D)%3BtotalNumberOfSeconds%20%2B%3D%20parseInt(timestamps%5Bi%5D.innerText.split(%22%3A%22)%5B1%5D)%3B%7Dif%20(totalNumberOfSeconds%20%3E%3D%2060)%20%7BtotalNumberOfMinutes%20%2B%3D%20Math.floor(totalNumberOfSeconds%20%2F%2060)%3BtotalNumberOfSeconds%20%3D%20totalNumberOfSeconds%20%25%2060%3B%7Dalert(%22Total%20time%20of%20playlist%3A%20%22%20%2B%20totalNumberOfMinutes%20%2B%20%22%3A%22%20%2B%20totalNumberOfSeconds)%7D)()
 
-/* SOURCE WITH CONSOLE DEBUG LINES
-var sumMin = 0, sumSec = 0, timestamps = document.querySelectorAll('.timestamp span');
+// ----------------------------
+// Source code with debug lines
+var totalNumberOfMinutes = 0, totalNumberOfSeconds = 0, timestamps = document.querySelectorAll('.timestamp span');
 
 for (let i=0; i < timestamps.length; i++) {
-    let min = 0, sec = 0, time;
-
-    // create temp time string
-    time = timestamps[i].innerText;
+    let videoDurationMinutes = 0, videoDurationSeconds = 0, videoTimestamp;
 
     // (object) timestamps[0] = <span aria-label="100 seconds">1:40</span>
-    min = parseInt(time.split(":")[0])
-    //console.log("min: " + min);
-    sumMin += min;
+    videoTimestamp = timestamps[i].innerText;
+    
+    videoDurationMinutes = parseInt(videoTimestamp.split(":")[0])
+    console.log("Video duration minutes portion: " + videoDurationMinutes);
+    totalNumberOfMinutes += videoDurationMinutes;
 
-    sec = parseInt(time.split(":")[1])
-    //console.log("sec: " + sec);
-    sumSec += sec;
+    videoDurationSeconds = parseInt(videoTimestamp.split(":")[1])
+    console.log("Video duration seconds portion: " + videoDurationSeconds);
+    totalNumberOfSeconds += videoDurationSeconds;
 }
 
-if (sumSec >= 60) {
-	// Add minutes
-	sumMin += Math.floor(sumSec / 60);
-
-	// Calculate remainder of sec
-	sumSec = sumSec % 60;
+if (totalNumberOfSeconds >= 60) {
+	totalNumberOfMinutes += Math.floor(totalNumberOfSeconds / 60);
+	totalNumberOfSeconds = totalNumberOfSeconds % 60;
 }
 
-//console.info("Total time of playlist: " + sumMin + ":" + sumSec);
-alert("Total time of playlist: " + sumMin + ":" + sumSec);
-*/
+console.info("Total time of playlist: " + totalNumberOfMinutes + ":" + totalNumberOfSeconds);
+alert("Total time of playlist: " + totalNumberOfMinutes + ":" + totalNumberOfSeconds);
