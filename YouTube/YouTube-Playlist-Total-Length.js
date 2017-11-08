082c05582ed0f8092cd88ae8c5adefcb11b41f8e
Jason Lee <jason.ferrier+github@gmail.com>
2017-11-08 12:26:07 -0800

    Fixed bug with last commit of the bookmarklet code

--- YouTube/YouTube-Playlist-Total-Length.js
+++ YouTube/YouTube-Playlist-Total-Length.js
@@ -1,5 +1,5 @@
 // Minified JS bookmarklet code created with http://mrcoles.com/bookmarklet/
-javascript:(function()%7Bvar%20totalNumberOfMinutes%20%3D%200%2C%20totalNumberOfSeconds%20%3D%200%2C%20timestamps%20%3D%20document.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')%3Bfor%20(let%20i%3D0%3B%20i%20%3C%20timestamps.length%3B%20i%2B%2B)%20%7Blet%20videoDurationMinutes%20%3D%200%2C%20videoDurationSeconds%20%3D%200%2C%20videoTimestamp%3B%2F%2F%20(object)%20timestamps%5B0%5D%20%3D%20%3Cspan%20aria-label%3D%22100%20seconds%22%3E1%3A40%3C%2Fspan%3EvideoTimestamp%20%3D%20timestamps%5Bi%5D.innerText%3BvideoDurationMinutes%20%3D%20parseInt(videoTimestamp.split(%22%3A%22)%5B0%5D)console.log(%22Video%20duration%20minutes%20portion%3A%20%22%20%2B%20videoDurationMinutes)%3BtotalNumberOfMinutes%20%2B%3D%20videoDurationMinutes%3BvideoDurationSeconds%20%3D%20parseInt(videoTimestamp.split(%22%3A%22)%5B1%5D)console.log(%22Video%20duration%20seconds%20portion%3A%20%22%20%2B%20videoDurationSeconds)%3BtotalNumberOfSeconds%20%2B%3D%20videoDurationSeconds%3B%7Dif%20(totalNumberOfSeconds%20%3E%3D%2060)%20%7BtotalNumberOfMinutes%20%2B%3D%20Math.floor(totalNumberOfSeconds%20%2F%2060)%3BtotalNumberOfSeconds%20%3D%20totalNumberOfSeconds%20%25%2060%3B%7Dalert(%22Total%20time%20of%20playlist%3A%20%22%20%2B%20totalNumberOfMinutes%20%2B%20%22%3A%22%20%2B%20totalNumberOfSeconds)%7D)()
+javascript:(function()%7Bvar%20totalNumberOfMinutes%20%3D%200%2C%20totalNumberOfSeconds%20%3D%200%2C%20timestamps%20%3D%20document.querySelectorAll('.ytd-thumbnail-overlay-time-status-renderer')%3Bfor%20(let%20i%3D0%3B%20i%20%3C%20timestamps.length%3B%20i%2B%2B)%20%7Blet%20videoDurationMinutes%20%3D%200%2C%20videoDurationSeconds%20%3D%200%2C%20videoTimestamp%3BvideoTimestamp%20%3D%20timestamps%5Bi%5D.innerText%3BvideoDurationMinutes%20%3D%20parseInt(videoTimestamp.split(%22%3A%22)%5B0%5D)%3BtotalNumberOfMinutes%20%2B%3D%20videoDurationMinutes%3BvideoDurationSeconds%20%3D%20parseInt(videoTimestamp.split(%22%3A%22)%5B1%5D)%3BtotalNumberOfSeconds%20%2B%3D%20videoDurationSeconds%3B%7Dif%20(totalNumberOfSeconds%20%3E%3D%2060)%20%7BtotalNumberOfMinutes%20%2B%3D%20Math.floor(totalNumberOfSeconds%20%2F%2060)%3BtotalNumberOfSeconds%20%3D%20totalNumberOfSeconds%20%25%2060%3B%7Dalert(%22Total%20time%20of%20playlist%3A%20%22%20%2B%20totalNumberOfMinutes%20%2B%20%22%3A%22%20%2B%20totalNumberOfSeconds)%7D)()
 
 // ----------------------------
 // Source code with debug lines
@@ -11,11 +11,11 @@ for (let i=0; i < timestamps.length; i++) {
     // (object) timestamps[0] = <span aria-label="100 seconds">1:40</span>
     videoTimestamp = timestamps[i].innerText;
     
-    videoDurationMinutes = parseInt(videoTimestamp.split(":")[0])
+    videoDurationMinutes = parseInt(videoTimestamp.split(":")[0]);
     console.log("Video duration minutes portion: " + videoDurationMinutes);
     totalNumberOfMinutes += videoDurationMinutes;
 
-    videoDurationSeconds = parseInt(videoTimestamp.split(":")[1])
+    videoDurationSeconds = parseInt(videoTimestamp.split(":")[1]);
     console.log("Video duration seconds portion: " + videoDurationSeconds);
     totalNumberOfSeconds += videoDurationSeconds;
 }
